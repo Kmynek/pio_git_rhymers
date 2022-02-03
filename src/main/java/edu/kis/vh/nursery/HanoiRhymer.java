a43e15d5512e7264f910aa9676db3ccc3f9ffe3f
kmynek <dawkoc@gmail.com>
2022-02-03 19:22:47 +0100

    1.5.6

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,13 +1,20 @@
 package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends defaultCountingOutRhymer {
-
+    /**
+     * counting out class
+     */
     int totalRejected = 0;
-
+    /**
+     * return total rejected int
+     */
     public int reportRejected() {
         return totalRejected;
     }
-
+    /**
+     * counting total rejected
+     * @param in input number to count in
+     */
     @Override
     public void countIn(final int in) {
         if (!callCheck() && in > peekaboo())
