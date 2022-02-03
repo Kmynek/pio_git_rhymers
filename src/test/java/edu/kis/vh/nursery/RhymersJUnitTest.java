cb401298948da9c86593c2647e0b7058c6c887f0
kmynek <dawkoc@gmail.com>
2022-02-03 19:20:26 +0100

    1.5.4

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -1,78 +1,94 @@
 package edu.kis.vh.nursery;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class RhymersJUnitTest {
+	final int STACK_CAPACITY = 12;
+	final int EMPTY_STACK_VALUE = -1;
+	defaultCountingOutRhymer rhymer;
+
+	@Before
+	public void init(){
+		rhymer = new defaultCountingOutRhymer();
+	}
 
 	@Test
 	public void testCountIn() {
-		defaultCountingOutRhymer rhymer = new defaultCountingOutRhymer();
-		int testValue = 4;
-		rhymer.countIn(testValue);
+		int expectedValue = 4;
+		rhymer.countIn(expectedValue);
 
 		int result = rhymer.peekaboo();
-		Assert.assertEquals(testValue, result);
+		Assert.assertEquals("Error. Expected value: "+expectedValue+ " result:" + result
+				,expectedValue,result);
 	}
 
 	@Test
 	public void testCallCheck() {
-		defaultCountingOutRhymer rhymer = new defaultCountingOutRhymer();
+
 		boolean result = rhymer.callCheck();
-		Assert.assertEquals(true, result);
+		Assert.assertTrue("Error. Expected value:"+ true + "result:" + result
+				,result);
 
 		rhymer.countIn(888);
 
 		result = rhymer.callCheck();
-		Assert.assertEquals(false, result);
+		Assert.assertFalse("Error. Expected value: "+ false + "result:" + result
+				,result);
 	}
 
 	@Test
 	public void testIsFull() {
-		defaultCountingOutRhymer rhymer = new defaultCountingOutRhymer();
-		final int STACK_CAPACITY = 12;
 		for (int i = 0; i < STACK_CAPACITY; i++) {
 			boolean result = rhymer.isFull();
-			Assert.assertEquals(false, result);
+			Assert.assertFalse("Error. Expected value: "+ false + "result:" + result
+					,result);
 			rhymer.countIn(888);
 		}
 
 		boolean result = rhymer.isFull();
-		Assert.assertEquals(true, result);
+		Assert.assertTrue("Error. Expected value:"+ true + "result:" + result
+				,result);
 	}
 
 	@Test
 	public void testPeekaboo() {
-		defaultCountingOutRhymer rhymer = new defaultCountingOutRhymer();
-		final int EMPTY_STACK_VALUE = -1;
 
 		int result = rhymer.peekaboo();
-		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+		Assert.assertEquals("Error. Expected value:" + EMPTY_STACK_VALUE + "result:" + result
+				, EMPTY_STACK_VALUE, result);
 
 		int testValue = 4;
 		rhymer.countIn(testValue);
 
 		result = rhymer.peekaboo();
-		Assert.assertEquals(testValue, result);
-		result = rhymer.peekaboo();
-		Assert.assertEquals(testValue, result);
+		Assert.assertEquals("Error. Expected value:" + testValue + "result:" + result
+				,testValue, result);
 	}
-
 	@Test
 	public void testCountOut() {
-		defaultCountingOutRhymer rhymer = new defaultCountingOutRhymer();
-		final int EMPTY_STACK_VALUE = -1;
+
 
 		int result = rhymer.countOut();
-		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+		Assert.assertEquals("Error. Expected value:" + EMPTY_STACK_VALUE + "result:" + result
+				,EMPTY_STACK_VALUE, result);
 
 		int testValue = 4;
 		rhymer.countIn(testValue);
 
 		result = rhymer.countOut();
-		Assert.assertEquals(testValue, result);
+		Assert.assertEquals("Error. Expected value:" + testValue + "result:" + result
+				,testValue, result);
+
 		result = rhymer.countOut();
-		Assert.assertEquals(EMPTY_STACK_VALUE, result);
+
+		Assert.assertEquals("Error. Expected value: " + EMPTY_STACK_VALUE + "result." + result
+				,EMPTY_STACK_VALUE, result);
 	}
+
 
 }
